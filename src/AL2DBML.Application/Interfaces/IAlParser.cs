d05249040e19b8d93d7123fc337ccd7fff3f77c1
OGR-67 <roosolivier@gmail.com>
2026-03-15 21:24:08 +0100

    Add file type detection and parsing functionality to AlParser

--- src/AL2DBML.Application/Interfaces/IAlParser.cs
+++ src/AL2DBML.Application/Interfaces/IAlParser.cs
@@ -1,8 +1,10 @@
+using AL2DBML.Core.Enums;
 using AL2DBML.Core.Models;
 
 namespace AL2DBML.Application.Interfaces;
 
 public interface IAlParser
 {
+    AlFileType DetectFileType(string alFileContent);
     DBMLEnum ParseEnum(string alEnumFileContent);
 }
--- src/AL2DBML.Parser/AlParser.cs
+++ src/AL2DBML.Parser/AlParser.cs
@@ -1,12 +1,44 @@
-﻿using AL2DBML.Application.Interfaces;
+﻿using System.Text.RegularExpressions;
+using AL2DBML.Application.Interfaces;
+using AL2DBML.Core.Enums;
 using AL2DBML.Core.Models;
 
 namespace AL2DBML.Parser;
 
 public class AlParser : IAlParser
 {
-    public DBMLEnum ParseEnum(string alEnumFileContent)
+    private static class Patterns
     {
-        throw new NotImplementedException();
+        public const string Enum = @"^\s*enum\s+\d+\s+(""[^""]+""|\w+)(\s+implements\s+(""[^""]+""|\w+)(\s*,\s*(""[^""]+""|\w+))*)?\s*{";
+        public const string EnumExtension = @"^\s*enumextension\s+\d+\s+(""[^""]+""|\w+)\s+extends\s+(""[^""]+""|\w+)";
+        public const string EnumValue = @"^\s*value\(\s*\d+;\s*(""[^""]+""|\w+)";
+        public const string Table = @"^\s*table\s+\d+\s+(""[^""]+""|\w+)";
+        public const string TableExtension = @"^\s*tableextension\s+\d+\s+(""[^""]+""|\w+)\s+extends\s+(""[^""]+""|\w+)";
+    }
+    private readonly OutputSchema _outputSchema = new();
+
+    public AlFileType DetectFileType(string content) => content switch
+    {
+        var c when Regex.IsMatch(c, Patterns.EnumExtension, RegexOptions.Multiline) => AlFileType.EnumExtension,
+        var c when Regex.IsMatch(c, Patterns.Enum, RegexOptions.Multiline) => AlFileType.Enum,
+        var c when Regex.IsMatch(c, Patterns.TableExtension, RegexOptions.Multiline) => AlFileType.TableExtension,
+        var c when Regex.IsMatch(c, Patterns.Table, RegexOptions.Multiline) => AlFileType.Table,
+        _ => AlFileType.Unknown
+    };
+
+    public DBMLEnum ParseEnum(string content)
+    {
+        var name = AlSyntaxHelper.ExtractMatch(content, Patterns.Enum);
+        var dbmlEnum = GetOrCreateEnum(name, out bool isNew);
+        dbmlEnum.Values = AlSyntaxHelper.ExtractAllMatches(content, Patterns.EnumValue);
+        if (isNew) _outputSchema.Enums.Add(dbmlEnum);
+        return dbmlEnum;
+    }
+
+    private DBMLEnum GetOrCreateEnum(string name, out bool isNew)
+    {
+        var existing = _outputSchema.Enums.FirstOrDefault(e => e.Name == name);
+        isNew = existing is null;
+        return existing ?? new DBMLEnum { Name = name, Values = [] };
     }
 }
